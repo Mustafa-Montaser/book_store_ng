214c9fe4caf940c40f51427230be4817dd5308b9
Mustafa Montaser <mustafamontaser13@gmail.com>
2025-04-30 15:22:06 +0300

    implement header and navbar component (style only)

--- book-store/src/app/modules/dashboard/components/dashboard-layout/dashboard-layout.component.ts
+++ book-store/src/app/modules/dashboard/components/dashboard-layout/dashboard-layout.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from '../../shared/components/header/header.component';
+import { NavbarComponent } from '../../shared/components/navbar/navbar.component';
 
 @Component({
-  selector: 'app-dashboard-layout',
-  imports: [],
-  templateUrl: './dashboard-layout.component.html',
-  styleUrl: './dashboard-layout.component.scss'
+    selector: 'app-dashboard-layout',
+    imports: [HeaderComponent, NavbarComponent],
+    templateUrl: './dashboard-layout.component.html',
+    styleUrl: './dashboard-layout.component.scss'
 })
 export class DashboardLayoutComponent {
 
--- book-store/src/app/modules/dashboard/shared/components/navbar/navbar.component.ts
+++ book-store/src/app/modules/dashboard/shared/components/navbar/navbar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
-  selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+    selector: 'app-navbar',
+    imports: [MatIconModule],
+    templateUrl: './navbar.component.html',
+    styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
